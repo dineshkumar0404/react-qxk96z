1e8ad5aa426c8707bdfeb0a388ba14f770f2d85b
dineshkumar0404 <dineshkumar.g@arkinfotec.com>
2022-08-17 21:15:49 +0530

    1 file modified

--- src/Addmodel.js
+++ src/Addmodel.js
@@ -47,3 +47,53 @@ function Addmodel(args) {
 }
 
 export default Addmodel;
+
+// import React, { Component } from 'react';
+// import { Modal, Button } from 'react-bootstrap';
+// import RegisterForm from './components/RegisterForm.js';
+
+// class Addmodel extends Component {
+//   state = {
+//     isOpen: false,
+//   };
+
+//   openModal = () => this.setState({ isOpen: true });
+//   closeModal = () => this.setState({ isOpen: false });
+
+//   saveEmpDetail(e) {
+//     this.props.passemployee(e);
+//     this.closeModal(e);
+//   }
+
+//   cancelForm(e) {
+//     this.closeModal(e);
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <div>
+//           <h1 style={{ marginTop: '10px', textAlign: 'center' }}>
+//             {' '}
+//             Employee List
+//           </h1>
+//           <Button
+//             color="primary"
+//             style={{ float: 'right' }}
+//             onClick={this.openModal}
+//           >
+//             Add Employee
+//           </Button>
+
+//           <Modal show={this.state.isOpen} onHide={this.closeModal}>
+//             <RegisterForm
+//               saveEmpDetail={(e) => this.saveEmpDetail(e)}
+//               cancelForm={(e) => this.cancelForm(e)}
+//             />
+//           </Modal>
+//         </div>
+//       </>
+//     );
+//   }
+// }
+// export default Addmodel;
